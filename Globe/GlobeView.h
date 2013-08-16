1d9f042eb0057c281550bf4ace6ace5f1453ca9b
John Brewer <jbrewer@jera.com>
2013-08-16 02:01:07 -0700

    Eventual replacement for legacy EAGLView class.

---
+++ Globe/GlobeView.h
@@ -0,0 +1,13 @@
+//
+//  GlobeView.h
+//  Globe
+//
+//  Created by John Brewer on 8/16/13.
+//  Copyright (c) 2013 Jera Design LLC. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+@interface GlobeView : GLKView
+
+@end
---
+++ Globe/GlobeView.m
@@ -0,0 +1,31 @@
+//
+//  GlobeView.m
+//  Globe
+//
+//  Created by John Brewer on 8/16/13.
+//  Copyright (c) 2013 Jera Design LLC. All rights reserved.
+//
+
+#import "GlobeView.h"
+
+@implementation GlobeView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
